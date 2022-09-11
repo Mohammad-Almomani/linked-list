68fe09a4c08ab757f1a79c5eb0e84e1f62f1542b
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-09-11 14:58:18 +0300

    added insert at index methode and tested it

--- __test__/ll.test.js
+++ __test__/ll.test.js
@@ -15,10 +15,11 @@ describe("Verify insert node at the beginning", () => {
   it("Test inserting node at the beginning", () => {
     const LL = new LinkedList();
     LL.insertAtBeginning(1);
-    expect(LL.head.value).toEqual(1);
+    expect(LL.head.value).toBe(1);
+    expect(LL.head.next).toBeNull();
     LL.insertAtBeginning(2);
-    expect(LL.head.value).toEqual(2);
-    expect(LL.head.next.value).toEqual(1);
+    expect(LL.head.value).toBe(2);
+    expect(LL.head.next.value).toBe(1);
     expect(LL.head.next.next).toBeNull();
   });
 });
@@ -63,7 +64,6 @@ describe("Verify delete middle node", () => {
   });
 });
 
-
 describe("Verify delete node by index", () => {
   it("Test deleting node by index in non empty linked list", () => {
     const LL = new LinkedList();
@@ -74,15 +74,41 @@ describe("Verify delete node by index", () => {
     LL.deleteByIndex(0);
 
     expect(LL.head.value).toEqual(1);
-    expect(LL.head.next).toBeNull(); 
-   });
-   
+    expect(LL.head.next).toBeNull();
+  });
+
   it("Test deleting middle node for one element linked list", () => {
     const LL = new LinkedList();
     LL.insertAtBeginning(1);
     LL.deleteByIndex(5);
     expect(LL.head.value).toEqual(1);
     expect(LL.head.next).toBeNull();
+  });
+});
 
+describe("Verify insert node by index", () => {
+  it("Test inserting node by index in non empty linked list", () => {
+    const LL = new LinkedList();
+    LL.insertAtBeginning(1);
+    LL.insertAtBeginning(2);
+    LL.insertAtBeginning(3);
+    LL.insertAtIdx(1, 4);
+    expect(LL.head.value).toEqual(3);
+    expect(LL.head.next.value).toEqual(4);
+    expect(LL.head.next.next.value).toEqual(2);
+    expect(LL.head.next.next.next.value).toEqual(1);
+    expect(LL.head.next.next.next.next).toBeNull();
+  });
+  it("Test inserting node by index in empty linked list", () => {
+    const LL = new LinkedList();
+    LL.insertAtIdx(1, 1);
+    expect(LL.head).toBeNull();
+  });
+
+  it("Test inserting node by index in linked list with one element", () => {
+    const LL = new LinkedList();
+    LL.insertAtBeginning(1);
+    LL.insertAtIdx(0, 2);
+    expect(LL.head.value).toEqual(2);
   });
 });
